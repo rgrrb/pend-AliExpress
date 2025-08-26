345b1dbe8e2d2cc4314f57044a54e003273f86a0
ROGER RIBEIRO DE OLIVEIRA <25132662@CFP127ALUNO.LOCAL>
2025-08-26 15:05:38 -0300

    atividade finalizada

--- app.js
+++ app.js
@@ -5,15 +5,31 @@ import products from './produtos_atualizados.json' with {type: 'json'}
 function getInfoProducts(products){
 
     const listaProdutos = document.getElementById('listaProdutos')
+
     const produtos = document.getElementById('produtos')
+
     const card = document.createElement('div')
+
     card.classList.add('card')
+
     const img = document.createElement('img')
+
     const nome = document.createElement('span')
+
     nome.classList.add('nome')
+
+    const precoAvaliacao = document.createElement('div')
+
+    precoAvaliacao.classList.add("precoAvaliacao") 
+
+    const avaliacao = document.createElement('p')
+
     const preco = document.createElement('span')
+
     preco.classList.add('preco')
+
     const descricao = document.createElement('span')
+
     descricao.classList.add('descricao')
 
     const path = `./imgProdutos/${products.imagem}`
@@ -22,11 +38,15 @@ function getInfoProducts(products){
     nome.textContent = products.nome
     preco.textContent = (`R$${products.preco}`)
     descricao.textContent = products.descricao
+    avaliacao.textContent = (`Avaliação ${products.classificacao*2}/10`)
+    precoAvaliacao.append(preco, avaliacao)
+    
 
-    card.append(img, nome, preco, descricao)
+    card.append(img, nome, precoAvaliacao, descricao)
     produtos.appendChild(card)
 
     console.log("teste")
 }
+
 
 products.forEach(getInfoProducts)
